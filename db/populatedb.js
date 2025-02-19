75e7926cbfcbc693868690fc78e1b11e22fc7881
Brandon Lin <brandonlin9716@gmail.com>
2025-02-19 16:34:30 +0800

    Move file reading into script

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,9 +2,9 @@ import pg from 'pg';
 import fs from 'fs';
 import { connectionString } from './connection.js';
 
-const sql = fs.readFileSync('./db/populatedb.sql').toString();
 async function main() {
     console.log('seeding...');
+    const sql = fs.readFileSync('./db/populatedb.sql',  { encoding: 'utf8' });
     const client = new pg.Client({ connectionString });
     await client.connect();
     await client.query(sql);
