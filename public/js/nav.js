b2989d262e23157f169b406030fcbc591f64b5b3
Brandon Lin <brandonlin9716@gmail.com>
2025-02-25 11:31:10 -0800

    Hide and unhide nav links on menu button click

---
+++ public/js/nav.js
@@ -0,0 +1,6 @@
+const menuButton = document.querySelector('.menu-button');
+const menuLinks = document.querySelector('.menu-links');
+
+menuButton.addEventListener('click', () => {
+    menuLinks.classList.toggle('hidden');
+})
