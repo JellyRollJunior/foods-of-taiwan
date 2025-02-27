e63997c661eca1b839980cd40c62f7c8d59d0578
Brandon Lin <brandonlin9716@gmail.com>
2025-02-27 09:33:13 -0800

    Validate form data before inserting food

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -1,4 +1,24 @@
 import * as db from '../db/queries.js';
+import { body, validationResult } from 'express-validator';
+
+const lengthErr = 'Must be between 1 and 25 characters';
+const descriptionErr = 'Must be between 1 and 250 characters';
+const categoryErr = 'Error selecting category';
+
+const validateFood = [
+    body('title')
+        .trim()
+        .isLength({ min: 1, max: 25 })
+        .withMessage(`Title ${lengthErr}`),
+    body('description')
+        .trim()
+        .isLength({ min: 1, max: 250 })
+        .withMessage(`Description ${descriptionErr}`),
+    body('categoryId')
+    .trim()
+    .isInt()
+    .withMessage(categoryErr),
+];
 
 const getFoodsPage = async (request, response) => {
     const foods = await db.getFoods();
@@ -18,12 +38,20 @@ const getAddFoodPage = async (request, response) => {
     });
 };
 
-const postAddFood = async (request, response) => {
-    const title = request.body.title;
-    const description = request.body.description;
-    const categoryId = request.body.categoryId;
-    await db.insertFood(title, description, categoryId);
-    response.redirect('/foods');
-};
+const postAddFood = [
+    validateFood,
+    async (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            console.log('oops! error');
+            return;
+        }
+        const title = request.body.title;
+        const description = request.body.description;
+        const categoryId = request.body.categoryId;
+        await db.insertFood(title, description, categoryId);
+        response.redirect('/foods');
+    },
+];
 
 export { getFoodsPage, getAddFoodPage, postAddFood };
