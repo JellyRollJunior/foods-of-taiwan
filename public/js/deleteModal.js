8da5ea16451320646d2cb5bd68633e9092475753
Brandon Lin <brandonlin9716@gmail.com>
2025-03-03 16:38:36 -0800

    Make Food card delete btn open prompt. Make modal delete btn delete food

--- public/js/deleteModal.js
+++ public/js/deleteModal.js
@@ -1,7 +1,11 @@
 const deleteModal = document.querySelector('#delete-modal');
+const deleteModalForm = document.querySelector('#delete-modal form');
+const deleteModalFoodTitle = document.querySelector('#delete-modal .deleted-item');
 
-const openDeleteModal = () => {
+const openDeleteModal = (action, title) => {
     deleteModal.classList.remove('hidden');
+    deleteModalForm.action = action;
+    deleteModalFoodTitle.textContent = title;
 };
 
 const closeDeleteModal = () => {
