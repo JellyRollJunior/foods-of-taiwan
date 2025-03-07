f4d4e01ebf7d5de3e74595af6a32840a10714864
Brandon Lin <brandonlin9716@gmail.com>
2025-03-06 17:23:46 -0800

    Add more test data to populate db script

--- scripts/populatedb.sql
+++ scripts/populatedb.sql
@@ -25,18 +25,22 @@ CREATE TABLE IF NOT EXISTS food_categories (
 
 -- Insert foods
 INSERT INTO foods (is_default_value, title, description) VALUES 
-    (TRUE, 'Pork blood rice cake', 'A rice cake that is made of congealed pork blood and rice (tastes much better than it sounds!). Normally steamed then dusted with finely chopped nuts, a thick soy sauce based sauce, and cilantro. Served on a stick and enjoyed with or without spicy sauce. A staple of night market stall food!'),
-    (TRUE, 'Oyster omlet', 'An egg/tapioca mixture with green onions that is pan fried with oysters. Often enjoyed with a thick soy sauce. The chewy texture of the egg tapioca omlet pairs wonderfully with the salty soy sauce and oyster flavor!'),
-    (TRUE, 'Roast duck', 'Whole roasted duck with it''s signature crispy skin! Typically, the chef will bring the entire duck out and cut pieces of the cripsy skin in front of the table. A variety of dishes are then made using the skin and the meat.'),
-    (TRUE, 'Guava juice', 'Juice made with guava; guava is a deliciously refreshing fruit with a big crispy crunch when bitten into.'),
-    (TRUE, 'Tea eggs', 'Wonderfully fragrant boiled eggs marinated in a tea based marinade. The surface of the egg is stained brown from the marinade infusing with the egg. Yummy!'),
+    (TRUE, 'Pork Blood Rice Cake', 'A rice cake that is made of congealed pork blood and rice (tastes much better than it sounds!). Normally steamed then dusted with finely chopped nuts, a thick soy sauce based sauce, and cilantro. Served on a stick and enjoyed with or without spicy sauce. A staple of night market stall food!'),
+    (TRUE, 'Oyster Omlet', 'An egg/tapioca mixture with green onions that is pan fried with oysters. Often enjoyed with a thick soy sauce. The chewy texture of the egg tapioca omlet pairs wonderfully with the salty soy sauce and oyster flavor!'),
+    (TRUE, 'Roast Duck', 'Whole roasted duck with it''s signature crispy skin! Typically, the chef will bring the entire duck out and cut pieces of the cripsy skin in front of the table. A variety of dishes are then made using the skin and the meat.'),
+    (TRUE, 'Guava Juice', 'Juice made with guava; guava is a deliciously refreshing fruit with a big crispy crunch when bitten into.'),
+    (TRUE, 'Tea Eggs', 'Wonderfully fragrant boiled eggs marinated in a tea based marinade. The surface of the egg is stained brown from the marinade infusing with the egg. Yummy!'),
+    (TRUE, 'Braised Pork on Rice', 'A classic comfort food staple of pork braised in soy sauce and served over rice! The sweet and salty sauce mixes with the rice to create a wonderful taste. Every family has their own recipe.'),
+    (TRUE, 'Taiwanese Beef Noodle Soup', 'This famous dish has two variants. A flavorful soy sauce based broth version and a clear delicately flavored version. Both are great but the author prefers the clear one slightly more.'),
+    (TRUE, 'Scallion pancake with egg', 'A common breakfast item consisting of a salty green onion pancake grilled with an egg on top. The pancake is salty, crispy, and has a soft center (unlike western pancakes). Typically enjoyed with thick soy sauce and soy milk.'),
     (FALSE, 'I am a test item', 'Please feel free to delete or edit me without guilt of removing neat information!'),
     (FALSE, 'I am a test item 2', 'Please feel free to delete or edit me without guilt of removing neat information!');
 
 INSERT INTO categories (is_default_value, title, description) VALUES
-    (TRUE, 'Street food', 'Cheap eats commonly sold from roadside stalls or in night markets'),
+    (TRUE, 'Street food', 'Cheap eats commonly sold from roadside stalls or in night markets.'),
     (TRUE, 'Fine dining', 'Dishes that are typically served at a restaurant and cost (usually) a pretty penny!'),
     (TRUE, 'Drinks/Snacks', 'Common drinks / snacks typically found in grocery stores or convenience stores.'),
+    (TRUE, 'Comfort food', 'Typical home cooked meals that please the soul or common dishes beloved by all!'),
     (FALSE, 'I am a test category 1', 'Please feel free to delete or edit me without guilt of removing neat information!'),
     (FALSE, 'I am a test category 2', 'Please feel free to delete or edit me without guilt of removing neat information!');
 
@@ -48,7 +52,13 @@ INSERT INTO food_categories (food_id, category_id) VALUES
     (5, 3),
     (5, 1),
     (6, 4),
-    (6, 2),
-    (6, 3),
-    (6, 1),
-    (7, 5);
+    (7, 4),
+    (8, 1),
+    (8, 3),
+    (9, 1),
+    (9, 2),
+    (9, 3),
+    (9, 4),
+    (9, 5),
+    (9, 6),
+    (10, 6);
