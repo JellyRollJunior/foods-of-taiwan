530670e43e181bbbab6902a8a462f7ae017b1886
Brandon Lin <brandonlin9716@gmail.com>
2025-03-12 13:27:47 -0700

    Remove port from DB connection string (for NEON DB configuration)

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
+const connectionString = `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}`;
 
 export { connectionString };
